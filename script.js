b8f34916ebd3942587299ec218698709e2d589fc
Roberto Riccioli <riccioli.rob@gmail.com>
2020-06-16 11:33:26 +0200

    CSS medievale

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
   // Dati personali:
 
-var name = prompt("Inserisci il tuo nome");
+var name = prompt("Dichiarate lo vostro nome, Messere/Madama");
 
-var familyName = prompt("Inserisci il tuo cognome");
+var familyName = prompt("Dichiarate lo nome della vostra casata");
 
-var age = prompt("Quanti anni hai?");
+var age = prompt("Dichiarate la vostra età");
 
 console.log(age)
 
+document.getElementById('age').innerHTML = "Nacque: " + age + " anni orsono.";
+
   // Dati del viaggio:
 
-var km = prompt("Quanti KM è distante la tua destinazione?");
+var km = prompt("Ordunque, di quanti KM è distante la vostra destinazione?");
+
+document.getElementById('name').innerHTML = "Distanza da percorrere: " + km + " km";
 
 // **Elaborazione del prezzo per km
 
@@ -20,7 +24,7 @@ var price = km * 0.21;
 
 // ** Mostriamo all'utente il prezzo NON scontato **
 
-document.getElementById('full-price').innerHTML = "La tariffa intera per il tuo percorso é " + price + " Euro";
+document.getElementById('full-price').innerHTML = "L'intera tariffa per lo vostro percorso in carrozza è " + price + " Fiorini";
 
 // ** Calcolo della scontistica **
 
@@ -39,4 +43,6 @@ console.log(finalPrice);
 
 // ** Ora mostriamo il prezzo eventualmente scontato
 
-document.getElementById('full-price').innerHTML = "La tariffa scontata per il tuo percorso é " + finalPrice.toFixed(2) + " Euro";
+document.getElementById('full-price').innerHTML = "La tariffa scontata per lo vostro percorso in carrozza è " + finalPrice.toFixed(2) + " Fiorini";
+
+document.getElementById('name').innerHTML = "Messere / Madama: " + name + " " + familyName;
